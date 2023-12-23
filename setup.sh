a12bf1127547fda128e42726afa3ebc54b4dad6a
CheesyFrappe <emirbalci360@gmail.com>
2023-12-23 22:23:09 +0300

    Fix typo in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
 sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get install libxvidcore-dev libx264-dev
-sudo apt-get install qt4-dev-tools libatlas-base-devsudo pip3 install opencv-python
+sudo apt-get install qt4-dev-tools libatlas-base-dev
+sudo pip3 install opencv-python
