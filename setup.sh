c84f948ef77a75064f5934facf6d5d8243ed7480
CheesyFrappe <emirbalci360@gmail.com>
2023-12-23 22:45:08 +0300

    Edit setup.sh, replaced new packages with 'qt4-dev-tools'

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+sudo apt-get update && sudo apt-get upgrade
+
 sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
 sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get install libxvidcore-dev libx264-dev
-sudo apt-get install qt4-dev-tools libatlas-base-dev
-sudo pip3 install opencv-python
+sudo apt-get install libgtk2.0-dev  # still testing
+sudo apt-get install libatlas-base-dev gfortran # still testing
+# sudo apt-get install qt4-dev-tools libatlas-base-dev
+sudo apt-get install python3-pip
+
+sudo pip install numpy
+sudo pip install opencv-python
