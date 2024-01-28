f7234dce3e84a2db783b4df7a4b0efd52d2a5173
CheesyFrappe <emirbalci360@gmail.com>
2024-01-28 22:45:27 +0300

    Edit setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,29 @@
-"""
-    will be filled.
-"""
+#!/bin/bash
+
+# Update the packages
+sudo apt-get update
+sudo apt-get upgrade
+
+# Check if an initial argument is passed for the target directory
+if [ $# -eq 0 ]; then
+    DATA_DIR="./"
+else
+    DATA_DIR="$1"
+fi
+
+# Install Python dependencies
+python3 -m pip install pip --upgrade
+python3 -m pip install -r requirements.txt
+
+# Set the file path for the TFLite model
+FILE=${DATA_DIR}/efficientdet_lite0.tflite
+
+# Check if the file already exists
+if [ ! -f "$FILE" ]; then
+# Download the TFLite model
+  curl \
+    -L 'https://storage.googleapis.com/download.tensorflow.org/models/tflite/task_library/object_detection/rpi/lite-model_efficientdet_lite0_detection_metadata_1.tflite' \
+    -o ${FILE}
+fi
+
+echo -e "Downloaded files are in {$DATA_DIR}"
